c4b623b896f79a6a145ad61563bd002937aa03fe
Prasanth Thomas Shaji <prasanthshaji@icloud.com>
2025-12-06 19:20:46 +0100

    2025: day06: part2

--- raoc2025/src/bin/day06.rs
+++ raoc2025/src/bin/day06.rs
@@ -1,4 +1,4 @@
-use ndarray::Array2;
+use ndarray::{Array2, ArrayView1};
 use raoc2025::utils::*;
 
 fn main() {
@@ -36,6 +36,73 @@ fn part1(input: &str) -> usize {
     sum
 }
 
+fn translate_sum(arr: ArrayView1<&str>) -> usize {
+    let n = arr[[1]].len();
+    let mut numbers: Vec<usize> = vec![0; n];
+    for region in arr {
+        for (i, c) in region.chars().rev().enumerate() {
+            if c.is_ascii_digit() {
+                numbers[i] *= 10;
+                numbers[i] += c.to_digit(10).unwrap() as usize;
+            }
+        }
+    }
+    numbers.into_iter().sum()
+}
+
+fn translate_mul(arr: ArrayView1<&str>) -> usize {
+    let n = arr[[1]].len();
+    let mut numbers: Vec<usize> = vec![0; n];
+    for region in arr {
+        for (i, c) in region.chars().rev().enumerate() {
+            if c.is_ascii_digit() {
+                numbers[i] *= 10;
+                numbers[i] += c.to_digit(10).unwrap() as usize;
+            }
+        }
+    }
+    numbers.into_iter().product()
+}
+
 fn part2(input: &str) -> usize {
-    input.lines().count()
+    let mut sum = 0;
+    let rows = input.lines().count() - 1;
+    let columns = input.lines().nth(0).unwrap().split_whitespace().count();
+    let mut grid: Array2<&str> = Array2::default((rows, columns));
+
+    let mut indexes: Vec<usize> = Vec::new();
+    let lines = input.lines().enumerate();
+    if let Some((_, ops)) = lines.last() {
+        for (index, _) in ops.match_indices("*") {
+            indexes.push(index);
+        }
+        for (index, _) in ops.match_indices("+") {
+            indexes.push(index);
+        }
+        indexes.push(ops.len()+3);
+    }
+    indexes.sort();
+
+    let lines = input.lines().enumerate();
+    for (row, line) in lines.clone().take(rows) {
+        let mut column = 0;
+        for pair in indexes.windows(2) {
+            if let Some(numbers) = line.get(pair[0]..pair[1]-1) {
+                grid[[row, column]] = numbers;
+                column += 1;
+            }
+        }
+    }
+
+    let lines = input.lines().enumerate();
+    if let Some((_, ops)) = lines.last() {
+        for (column, op) in ops.split_whitespace().enumerate() {
+            match op {
+                "*" => sum += translate_mul(grid.column(column)),
+                "+" => sum += translate_sum(grid.column(column)),
+                _ => panic!("oh no!"),
+            }
+        }
+    }
+    sum
 }
